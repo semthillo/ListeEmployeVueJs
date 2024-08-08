9088af4d01fb724da30a1c4d6be95d665766026f
thillo <salysem90@gmail.com>
2024-08-08 22:27:25 +0200

    commit liste

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="titre">Liste Des Employés : </h1>
+
+ <ol class="liste">
+  <li >
+    Mariem Dianifaba
+  </li>
+  <li>
+    Assa Baradji
+  </li>
+  <li>
+    Big Traoré
+  </li>
+  <li>
+    Mohamed Abdallahi
+  </li>
+  <li>
+    Ousmane Ly
+  </li>
+  <li>
+    Mohamed Bankolé
+  </li>
+  <li>
+    Abdrahmane Thimbo
+  </li>
+  <li>
+    Mamoudou Ba
+  </li>
+  <li>
+    Fatima Zahra
+  </li>
+  <li>
+    Thillo Seme
+  </li>
+ </ol>
+</template>
+
+<style scoped>
+
+</style>
